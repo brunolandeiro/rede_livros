dd8d3c1f1a4be2b07bbe6aa436ceaf45cef1293f
Bruno Landeiro <bruno.landeiro@quality.com.br>
2018-02-09 15:21:29 -0200

    pg login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="w3-row">
+    <div class="w3-col s1 m2 l3"><p></p></div>
+    <div class="w3-col s10 m8 l6">
+        <div class="w3-card-2">
+            <div class="w3-container w3-theme-d2">
+                <h2>Login</h2>
             </div>
+            <form class="w3-container" method="POST" action="{{ route('login') }}">
+                <br>
+                <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" >E-mail</label>
+                    <input class="w3-input" name="email" id="email" type="email" value="{{ old('email') }}" required autofocus>
+                </div>
+                @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+
+                <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="email" >Senha</label>
+                    <input class="w3-input" name="password" id="password" type="password" value="{{ old('password') }}" required autofocus>
+                </div>
+                @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+                <p>
+                    <button type="submit" class="w3-btn w3-theme-d2">Login</button>
+                    <a class="w3-btn" href="{{ route('password.request') }}">Esqueceu a senha?</a>
+                </p>
+            </form>
         </div>
     </div>
+    <div class="w3-col s1 m2 l3"><p></p></div>
 </div>
 @endsection
